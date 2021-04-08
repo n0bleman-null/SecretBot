3297e437c92d9d6819196c81100d9f42c5d9f57e
n0bleman <37957247+n0bleman-null@users.noreply.github.com>
2021-04-08 22:01:55 +0300

    Delete Configuration.cs

--- Configuration.cs
+++
@@ -1,8 +0,0 @@
-namespace TelegramBot
-{
-    public static class Configuration
-    {
-        public static readonly string BotToken = "1524669807:AAFZaJITRdSI6m69pgUbYA9ZgmKqp2Gb66s";
-        public static readonly string BotName = "@patajemnyhitlerbot";
-    }
-}
